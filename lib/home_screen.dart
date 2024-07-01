94aceee7746a215b24ca1bb6aad09a14dafc943c
Lovejeetsingh2003 <135810389+Lovejeetsingh2003@users.noreply.github.com>
2024-07-01 15:17:39 +0530

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Bottom Navigation App",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        color: _currentIndex == 0 ? Colors.amber : Colors.blue,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (value) {
+          _currentIndex = value;
+          setState(() {});
+        },
+        backgroundColor: Colors.black,
+        selectedLabelStyle: TextStyle(color: Colors.white),
+        selectedIconTheme: IconThemeData(size: 40),
+        showSelectedLabels: true,
+        selectedItemColor: Colors.pink.shade100,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              label: "home"),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.bar_chart_rounded,
+              color: Colors.white,
+            ),
+            label: "chart",
+          )
+        ],
+      ),
+    );
+  }
+}
